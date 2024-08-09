43f56788542da709efa0c8d0a0cb2a2ce44a871c
laura4343 <123laurafarias@gmail.com>
2024-08-09 10:43:51 -0300

    first commit

---
+++ src/main/java/com/example/hestiaapimongo/HestiaApiMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hestiaapimongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HestiaApiMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HestiaApiMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hestiaapimongo/HestiaApiMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hestiaapimongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HestiaApiMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
