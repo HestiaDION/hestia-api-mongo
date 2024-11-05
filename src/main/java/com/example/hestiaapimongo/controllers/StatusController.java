25ac99ef163bb386a27bc62228a3014fdd504889
sophiekumagai <sophie.satie20@gmail.com>
2024-11-05 00:35:48 -0300

    alterando retorno status

--- src/main/java/com/example/hestiaapimongo/controllers/StatusController.java
+++ src/main/java/com/example/hestiaapimongo/controllers/StatusController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +28,12 @@ public class StatusController {
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor",
                     content = @Content())
     })
-    public Map<String, String> getStatus() {
+    public ResponseEntity<Map<String, String>> getStatus() {
         // Cria um mapa com informações de status
         Map<String, String> status = new HashMap<>();
         status.put("status", "API is running");
 
         // Retorna o mapa como JSON
-        return status;
+        return new ResponseEntity<>(status, HttpStatus.OK);
     }
 }
